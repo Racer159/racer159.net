7e8e373ab20e8962d80c0e5486b8a282170651ef
Wayne Starr <racer159@live.com>
2022-02-20 17:46:30 -0600

    feat: make pages dynamic and more like php

--- src/index.ts
+++ src/index.ts
@@ -24,18 +24,20 @@ async function handleRequest(event: FetchEvent) {
       };
     }
 
-    if (new URL(event.request.url).pathname === '/') {
-      options.mapRequestToAsset = req => new Request(`${new URL(req.url).origin}/index.htm`, req);
+    const url = new URL(event.request.url);
+
+    if (url.pathname === '/') {
+      options.mapRequestToAsset = req => new Request(`${url.origin}/home.htm`, req);
     }
 
-    const page = await getAssetFromKV(event, options);
+    const pageResponse = await getAssetFromKV(event, options);
 
     // allow headers to be altered
-    const response = new Response(page.body, page);
+    const response = await handlePage(url, event, pageResponse);
 
     response.headers.set('X-XSS-Protection', '1; mode=block');
     response.headers.set('X-Content-Type-Options', 'nosniff');
-    response.headers.set('X-Frame-Options', 'DENY');
+    response.headers.set('X-Frame-Options', 'SAMEORIGIN');
     response.headers.set('Referrer-Policy', 'unsafe-url');
     response.headers.set('Feature-Policy', 'none');
 
@@ -45,9 +47,13 @@ async function handleRequest(event: FetchEvent) {
     // if an error is thrown try to serve the asset at 404.htm
     if (!DEBUG) {
       try {
-        const notFoundResponse = await getAssetFromKV(event, {
+        const url = new URL(event.request.url);
+
+        let notFoundResponse = await getAssetFromKV(event, {
           mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.htm`, req),
         });
+
+        notFoundResponse = await handlePage(new URL(url.origin + '/404.htm'), event, notFoundResponse);
 
         return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
       } catch (e) {
@@ -62,4 +68,29 @@ async function handleRequest(event: FetchEvent) {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function handle500(e: any) {
   return new Response(e.message || e.toString(), { status: 500 });
+}
+
+/**
+ * Inject page content based on URL to make page dynamic
+ */
+ async function handlePage(url: URL, event: FetchEvent, page: Response) {
+  if (url.pathname === '/' || (url.pathname.endsWith('.htm')  && !url.pathname.startsWith('/net/'))) {
+    // retrieve the index page
+    const index = await getAssetFromKV(event, {
+      mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.htm`, req),
+    });
+
+    // replace index {{page}} with real page
+    let indexBody = await index.text();
+    const pageBody = await page.text();
+    indexBody = indexBody.replace('{{page}}', pageBody);
+
+    const resp = new Response(indexBody, page);
+    resp.headers.set('Content-Type', 'text/html; charset=UTF-8');
+
+    return resp;
+  } else {
+    // return the default response
+    return new Response(page.body, page);
+  }
 }
